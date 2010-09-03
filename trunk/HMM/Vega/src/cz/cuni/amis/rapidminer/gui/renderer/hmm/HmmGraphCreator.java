f36d5b9fd2d88e85faadfd5f228fca0e31f785f5
rudolf.kadlec <rudolf.kadlec@88cfbb11-6438-5b35-ae81-3a4d98593ec5>
2010-09-03 20:15:44 +0000

    Initial commit. Operators for BaumWelch, KMeans and direct inference from fully observed data should be working but this is devel version only. It is not intended for real use.

---
+++ trunk/HMM/Vega/src/cz/cuni/amis/rapidminer/gui/renderer/hmm/HmmGraphCreator.java
@@ -0,0 +1,91 @@
+package cz.cuni.amis.rapidminer.gui.renderer.hmm;
+
+import com.rapidminer.gui.graphs.GraphCreatorAdaptor;
+import cz.cuni.amis.rapidminer.operator.hmm.HMMModel;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author ik
+ */
+public class HmmGraphCreator extends GraphCreatorAdaptor {
+
+    protected HMMModel hmm;
+
+    public HmmGraphCreator(HMMModel hmm) {
+        this.hmm = hmm;
+    }
+
+    public Graph<String, String> createGraph() {
+        Graph<String, String> graph = new DirectedSparseGraph<String, String>();
+
+        for (int i = 0; i < hmm.getHmm().nbStates(); i++) {
+            graph.addVertex(Integer.toString(i));
+        }
+
+        for (int i = 0; i < hmm.getHmm().nbStates(); i++) {
+            for (int j = 0; j < hmm.getHmm().nbStates(); j++) {
+                graph.addEdge(i + "->" + j, Integer.toString(i), Integer.toString(j));
+            }
+        }
+        return graph;
+    }
+
+    @Override
+    public String getEdgeName(String id) {
+        return "";
+    }
+
+    @Override
+    public String getVertexName(String id) {
+        return hmm.getStateName(Integer.parseInt(id));
+    }
+
+    @Override
+    public double getEdgeStrength(String id) {
+        String[] indices = id.split("->");
+        int i = Integer.parseInt(indices[0]);
+        int j = Integer.parseInt(indices[1]);
+        return hmm.getHmm().getAij(i, j);
+    }
+
+    @Override
+    public int getEdgeShape() {
+        return EDGE_SHAPE_QUAD_CURVE;
+    }
+
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    @Override
+    public String getVertexToolTip(String id) {
+        int i = Integer.parseInt(id);
+        // get probs of state transitions
+        StringBuilder builder = new StringBuilder();
+        for (int j =0; j < hmm.getHmm().nbStates(); j++) {
+            builder.append(hmm.getStateName(j))
+                    .append(":")
+                    .append(df.format(hmm.getHmm().getAij(i, j)))
+                    .append(", ");
+        }
+        
+        String obsProb = hmm.getHmm().getOpdf(i).toString();
+        return builder.toString() + "\n" + obsProb;
+    }
+
+
+
+
+/*
+    	@Override
+	public EdgeLabel<String, String> getEdgeLabelRenderer() {
+		return new TreeModelEdgeLabelRenderer<String,String>();
+	}
+
+    @Override
+	public VertexLabel<String, String> getVertexLabelRenderer() {
+        return new TreeModelNodeLabelRenderer<String, String>(this);
+    }
+ */
+}
---
+++ trunk/HMM/Vega/src/cz/cuni/amis/rapidminer/operator/hmm/HMMModel.java
@@ -0,0 +1,163 @@
+package cz.cuni.amis.rapidminer.operator.hmm;
+
+import be.ac.ulg.montefiore.run.jahmm.Hmm;
+import be.ac.ulg.montefiore.run.jahmm.ObservationInteger;
+import com.rapidminer.example.Attribute;
+import com.rapidminer.example.Attributes;
+import com.rapidminer.example.Example;
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.example.table.NominalMapping;
+import com.rapidminer.example.table.PolynominalAttribute;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.learner.PredictionModel;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author ik
+ */
+public class HMMModel extends PredictionModel {
+
+    /** HMM used for prediction. */
+    Hmm hmm;
+    /**
+     * Maps state indexes to human readable names.
+     */
+    NominalMapping stateNameMapping = null;
+
+    public HMMModel(Hmm hmm, ExampleSet es) {
+        super(es);
+        this.hmm = hmm;
+        PolynominalAttribute labelAtr = (PolynominalAttribute) es.getAttributes().get(Attributes.LABEL_NAME);
+        this.stateNameMapping = labelAtr.getMapping();
+    }
+
+    public HMMModel(Hmm hmm, ExampleSet es, NominalMapping stateNameMapping) {
+        super(es);
+        this.hmm = hmm;
+        PolynominalAttribute labelAtr = (PolynominalAttribute) es.getAttributes().get(Attributes.LABEL_NAME);
+        this.stateNameMapping = stateNameMapping;
+    }
+
+    public Hmm getHmm() {
+        return hmm;
+    }
+    HMMAlg currentAlgorithm = HMMAlg.VITERBI;
+    protected static final String ALG_KEY = "algorithm";
+
+    @Override
+    public void setParameter(String key, Object value) throws OperatorException {
+        if (key.equals(ALG_KEY)) {
+            try {
+                currentAlgorithm = HMMAlg.valueOf(value.toString());
+            } catch (IllegalArgumentException e) {
+                throw new OperatorException("Algorithm - " + value + " - not supported.", e);
+            }
+        }
+    }
+
+    @Override
+    public ExampleSet performPrediction(ExampleSet es, Attribute atrbt) throws OperatorException {
+
+        Attribute clusterAtr = es.getAttributes().getCluster();
+
+        switch (currentAlgorithm) {
+            case VITERBI:
+                return viterbi(es);
+            default:
+                throw new OperatorException("No valid algorithm set.");
+        }
+
+        //es = decorateWithPredWindow(es, clusterAssignment);
+
+    }
+
+    protected ExampleSet forwardBackward(ExampleSet es) {
+        //TODO hmm.probability(null);
+        return es;
+    }
+
+    protected ExampleSet viterbi(ExampleSet es) {
+        // translate the cluster values
+        List<ObservationInteger> clustersSequence = new ArrayList<ObservationInteger>(es.size());
+        PolynominalAttribute observAtr = (PolynominalAttribute) es.getAttributes().get(Attributes.CLUSTER_NAME);
+        Iterator<Example> it = es.iterator();
+        int observationIndex = -1;
+        while (it.hasNext()) {
+            observationIndex = observAtr.getMapping().mapString(it.next().getValueAsString(observAtr));
+            clustersSequence.add(new ObservationInteger(observationIndex));
+        }
+
+        // compute the most likely sequence of hidden states
+        int[] hiddenStates = hmm.mostLikelyStateSequence(clustersSequence);
+
+        // set the sequence as predicted label in ES
+        Iterator<Example> itES = es.iterator();
+        int i = 0;
+        while (itES.hasNext()) {
+            itES.next().setPredictedLabel(hiddenStates[i++]);
+        }
+        return es;
+    }
+
+    ExampleSet decorateWithPredWindow(ExampleSet es, int[] clusterAssignment) {
+        // number of observations to take into account
+        return es;
+        /*        int WINDOW_SIZE = 3;
+
+        if(WINDOW_SIZE <= es.getExampleTable().size()) throw new RuntimeException("ExampleSet shorter than time window.");
+
+        Attribute surpriseAtr = AttributeFactory.createAttribute("surprise", Ontology.REAL);
+        es.getAttributes().addRegular(surpriseAtr);
+        es.getExampleTable().addAttribute(surpriseAtr);
+
+        
+        List<ObservationInteger> obs = new LinkedList<ObservationInteger>();
+        Iterator<Example> exampleIt = es.iterator();
+        for(int i = 0; i < WINDOW_SIZE; i++) {
+        exampleIt.next();
+        obs.add(new ObservationInteger(clusterAssignment[i]));
+        }
+        // init finished
+
+        // computation loop
+        while(exampleIt.hasNext()) {
+        Example e = exampleIt.next();
+        // compute surprise for current time step
+        int[] stateSeq = hmm.mostLikelyStateSequence(obs);
+        //TODO hmm.getAij(WINDOW_SIZE, WINDOW_SIZE)stateSeq[WINDOW_SIZE-1]
+
+
+        // advance to the next time step
+        }
+        return es;
+         *
+         */
+    }
+
+    @Override
+    public String toString() {
+        return hmm.toString();
+    }
+
+    /**
+     * @param index Index of the state.
+     * @return Name of the state.
+     */
+    public String getStateName(int index) {
+        return stateNameMapping.mapIndex(index);
+    }
+
+    public NominalMapping getStateNameMapping() {
+        return stateNameMapping;
+    }
+
+
+    enum HMMAlg {
+
+        VITERBI,
+        FORWARD_BACKWARD,
+    }
+}
